a8a0efb04f1b94c01fe2af168045dcd1f9e380f3
Amanda Sarenana <59600132+AmandaSarenana@users.noreply.github.com>
2020-01-21 00:23:40 -0800

    Gaddis_Ch2.1

---
+++ Hmwk/Assignment_1/Gaddis_Ch2.1/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Amanda Sarenana
+ * Created on January 12, 2020, 1:00 PM
+ * Purpose:  C++ Template to be copied and utilized
+ * for homework, projects, exams!
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - No Global Variables
+//Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variable Data Types and Constants
+    int first=50;
+    int second=100;
+    int total;
+    
+    //Initialize Variables
+    cout<<"The first integer number: "<<first;
+    cout<<"\nThe second integer number: "<<second;
+    
+    //Process or map Inputs to Outputs
+    total=first+second;
+    
+    //Display Outputs
+    cout<<"\nThe sum of first and second number:"<<total<<endl;
+    
+    //Exit stage right!
+    return 0;
+}
